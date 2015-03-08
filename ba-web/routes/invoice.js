c7105c4721b2713a530fa1e65fee12e101b0754a
shubho-acc <shubho@mail-acc.com>
2015-03-08 13:08:29 +0530

    Ba Web: View partials added

--- ba-web/routes/invoice.js
+++ ba-web/routes/invoice.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
-var pdf = require('html-pdf');
-var htmlToPdf = require('html-to-pdf');
-
-
 router.get('/', function(req, res) {
 	console.log("Rendering invoice save page...");
 	var currentUser = req.session.user ? JSON.parse(req.session.user) : null;	
