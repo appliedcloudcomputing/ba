ebf02d96c573766ec16feacc895db2db42338490
Shubho Pramanik <shubho@mail-acc.com>
2015-03-14 18:38:31 +0530

    named all elemeted invoice

--- ba-web/routes/invoice.js
+++ ba-web/routes/invoice.js
@@ -11,4 +11,36 @@ router.get('/', function(req, res) {
 	}
 });
 
+
+router.post('/save', function(req, res) {
+	console.log('Saving Invoice...');
+	var currentUser = req.session.user ? JSON.parse(req.session.user) : null;
+	if(currentUser) {
+		var data = {'id': req.body.id,
+					'name':req.body.clientName,
+					'description':req.body.description, 
+					'rate':req.body.rate, 
+					'uom':req.body.uom};
+
+		Parse.Cloud.run('saveProduct', data, {
+			success: function(message) {
+				var response = {
+					message: message,
+					status: 200
+				}
+				res.end(JSON.stringify(response));
+			},
+			error: function(error) {
+				var response = {
+					message: error.message,
+					status: error.code
+				}
+				res.end(JSON.stringify(response));
+			}
+		});
+	} else {
+		res.redirect('/login');
+	} 
+});
+
 module.exports = router;
